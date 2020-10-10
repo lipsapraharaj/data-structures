e6948e551d00a853dd0b2f8d123cc3146444bc99
lipsapraharaj <69561010+lipsapraharaj@users.noreply.github.com>
2020-10-10 22:22:58 +0530

    Update MaxHeapify.cpp

--- Heaps/MaxHeapify.cpp
+++ Heaps/MaxHeapify.cpp
@@ -7,9 +7,9 @@ void max_heapify(int *a, int i, int n)
     int j, temp;
     temp = a[i];
     j = 2*i;
-    while (j <= n)
+    while (j < n)
     {
-        if (j < n && a[j+1] > a[j])
+        if (j < n-1 && a[j+1] > a[j])
             j = j + 1;
         if (temp > a[j])
             break;
